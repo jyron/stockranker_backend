d9438e31ce1b5686a4348e432d87c04665e4c172
jyron <jyron.dev@gmail.com>
2023-12-09 14:51:19 -0600

    Add get_stock function

--- app/crud/stocks.py
+++ app/crud/stocks.py
@@ -3,6 +3,11 @@
 from app.models.stock import Stock
 
 
+async def get_stock(ticker):
+    stock = await Stock.find_one({"ticker": ticker})
+    return stock
+
+
 async def get_all_stocks():
     stocks = await Stock.find().to_list(length=1000)
     return stocks
