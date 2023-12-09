99ca8350a2de9a5fa7f83083784ed7809897d697
jyron <jyron.dev@gmail.com>
2023-12-09 15:05:54 -0600

    organize imports

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from contextlib import asynccontextmanager
 import logging
+from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 
 from app.database import init_database
+from app.routers.stocks import stock_router
 from app.routers.users import user_router
 from app.scheduler import setup_scheduler
 from app.update_service import update_router
-from app.routers.stocks import stock_router
 
 
 @asynccontextmanager
@@ -19,7 +19,6 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-logging.basicConfig(level=logging.DEBUG)
 app.include_router(user_router, prefix="/api/v0")
 app.include_router(stock_router, prefix="/api/v0", tags=["stocks"])
 app.include_router(update_router, prefix="/api/v0", tags=["update"])
