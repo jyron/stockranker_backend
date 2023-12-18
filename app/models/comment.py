a0d754eb5a98a0c289370e05321fe54a4ef26e01
jyron <jyron.dev@gmail.com>
2023-12-17 23:58:08 -0600

    Added ObectID to comment model

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,9 +1,9 @@
 from typing import Optional, List
-from beanie import Document
+from beanie import Document, PydanticObjectId
 
 
 class Comment(Document):
-    user_id: str  # Assuming you have a user ID for identifying the commenter
+    user_id: PydanticObjectId  # Assuming you have a user ID for identifying the commenter
     content: str
     likes: Optional[int] = 0
     dislikes: Optional[int] = 0
